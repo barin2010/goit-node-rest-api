0cbce37e10e5da428dc1b735c60d051bc07950d1
barin2010 <uyrirudenko@gmail.com>
2024-03-22 21:24:10 +0200

    Update verifyContactOwner.js

--- middlewares/verifyContactOwner.js
+++ middlewares/verifyContactOwner.js
@@ -4,14 +4,11 @@ import { contactsService } from "../services/contactsServices.js";
 const verifyContactOwner = async (req, res, next) => {
   try {
     const userId = req.user._id.toString();
-    console.log("user id=", userId);
 
     const contactId = req.params.id;
-    console.log("id params=", contactId);
 
     const contact = await contactsService.getContactById(contactId);
     const contactOwner = contact.owner.toString();
-    console.log("owner=", contactOwner);
 
     if (!contact) {
       throw HttpError(404, "Contact not found");
